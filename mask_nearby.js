247026277f694c44a106cbad83976de058d99f61
Woo-Yeol <king199276@gmail.com>
2021-01-15 00:32:56 +0900

    Update Mask_Nearby

--- mask_nearby.js
+++ mask_nearby.js
@@ -1,3 +1,4 @@
+// 지도 표시하기
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
     center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
@@ -5,11 +6,13 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+
 var ps = new kakao.maps.services.Places(); 
 
 // 마스크 데이터 API 주소
 let base_mask_url = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?";
-// 2020.08 현재는 지원하지 않는 API
+// 2020.08 | 2021.01 현재는 지원하지 않는 API
 
 // 버튼 누르거나 Enter 눌렀을 때 검색이 되도록 만들기
 let search_btn = document.querySelector(".search-btn");
@@ -18,7 +21,7 @@ console.log(search_bar);
 console.log(search_btn);
 
 search_btn.addEventListener("click", () => {
-    let keyword = search_bar.value;
+    let keyword = search_bar.nodeValue;
     if(keyword) {
     console.log(keyword + " 검색하셨습니다.");
     keywordSearch(keyword);
@@ -42,6 +45,9 @@ function keywordSearch(keyword){
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
 async function keywordSearchCallback (data, status, pagination) {
     if (status === kakao.maps.services.Status.OK) {
+
+    // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+    // LatLngBounds 객체에 좌표를 추가합니다.
     const center = new kakao.maps.LatLng(data[0].y, data[0].x); 
     map.setCenter(center);
     const maskData = getMaskData(data[0].y, data[0].x);
